69417b9b771d77daaa903351e75eb15256cc943f
sandsip <sandsip@gmail.com>
2018-11-18 12:42:13 +0530

    Revert "hi"
    This reverts commit e25efae122c3e92333db8594ad2b9f7b9aa3366a.

---
+++ MvcMusicStore/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MvcMusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
